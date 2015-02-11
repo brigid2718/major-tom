9f5ab65ee1baf42c014761eba29db990ed901a8f
Brigid2718 <bridotson@gmail.com>
2015-02-11 14:40:48 -0800

    sound loops is toggled on button press

--- stepping-through-the-door.py
+++ stepping-through-the-door.py
@@ -3,14 +3,16 @@
 # modules to import
 import requests
 import time
-
+import RPi.GPIO as GPIO
 import sys
+import pygame
 sys.path.append('/home/pi/rpi_ws281x/python')
-sys.path.append('/home/pi/adxl345-python')
+# sys.path.append('/home/pi/adxl345-python')
 
 from neopixel import *
-from adxl345 import ADXL345
-from random import randint
+
+# from adxl345 import ADXL345
+# from random import randint
 
 # initiate light strip and load light strip functions
     # there should be a strip object that can display a pattern
@@ -39,8 +41,8 @@ LED_INVERT     = False   # True to invert the signal (when using NPN transistor 
 # light_strip = LightStrip(input_list)
 # light_strip.strip.begin()
 
-light_strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
-light_strip.begin()
+# light_strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+# light_strip.begin()
 
 def colorWipe(strip, color, wait_ms=50):
     """Wipe color across display a pixel at a time."""
@@ -56,9 +58,13 @@ def teleport(strip):
 # initiate sensor and load sensor reading functions
     # there should be a sensor object that has a is_triggered attribute
 class Sensor:
-    def __init__(self):
+    def __init__(self, pin=18):
         self.is_triggered = False
-        self.device = ADXL345()
+        self.pin = pin
+
+    def button_setup(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def trigger(self):
         self.is_triggered = True
@@ -66,26 +72,33 @@ class Sensor:
     def untrigger(self):
         self.is_triggered = False
 
-    def check_input(self):
-        prev_reading = self.device.getAxes(False)
-        print prev_reading
-        time.sleep(0.1)
-        current_reading = self.device.getAxes(False)
-        print current_reading
-        if abs(current_reading['x'] - prev_reading['x']) > 2:
-            print abs(current_reading['x'] - prev_reading['x'])
-            self.trigger()
-        else:
-            self.untrigger()
+    def check_for_trigger(self, previous_sensor_state, current_sensor_state):
+        d_sensor_state_dt = abs(current_sensor_state - previous_sensor_state)
+        if d_sensor_state_dt > 0:
+            if current_sensor_state == 0:
+                print "button is pressed down"
+            elif current_sensor_state == 1:
+                print "button released"
+                self.trigger()
 
-
-# Here is the setup of the sensor (dummy sensor right now)
+# Here is the setup of the sensor (button right now)
 
 sensor = Sensor()
-
+sensor.button_setup()
 
 # initiate sound control and load functions
     # there should be a sounds object that can play and pause
+soundfile = "teleport.wav"
+pygame.init()
+pygame.mixer.music.load(soundfile)
+pygame.mixer.music.set_volume(0.1)
+
+def toggle_music():
+    if pygame.mixer.music.get_busy():
+        pygame.mixer.music.stop()
+    else:
+        pygame.mixer.music.play(-1)
+
 # initiate ground control communications
     # there should be a ground_control object that has attributes light_permission
     # and sound_permission
@@ -113,12 +126,20 @@ ground_control = GroundControl("826dev")
 
 # while loop for running
 
+previous_sensor_state = GPIO.input(sensor.pin)
+
 while True:
+    current_sensor_state = GPIO.input(sensor.pin)
     # listen for sensor trigger
-    sensor.check_input()
+    sensor.check_for_trigger(previous_sensor_state, current_sensor_state)
     if sensor.is_triggered:
+        print "in triggered block"
         # check api for light and sound state as a condition of playing
         # sounds.play() if sound_permission(ground_control) else 0
-        teleport(light_strip) if ground_control.light_permission() else 0
+        toggle_music() if ground_control.light_permission() else 0
+        # teleport(light_strip) if ground_control.light_permission() else 0
+        # untrigger the sensor state
+        sensor.untrigger()
         # stop the sounds if they are currently playing (because the lights are now done)
         # sounds.stop() if sounds.are_playing()
+    previous_sensor_state = current_sensor_state
