616941b865f0f8ea0e512bffa028c3900095c4ef
Bri <bridotson@gmail.com>
2015-02-15 11:46:29 -0800

    make pattern init into list comprehension that makes each pixel its own object

--- pattern-lib/seattle.py
+++ pattern-lib/seattle.py
@@ -2,6 +2,8 @@
 
 """
 
+import pdb
+
 class Pixel():
     def __init__(self,  color=Color(0,0,0), brightness=1):
         self.color = color
@@ -12,13 +14,13 @@ def initpattern(strip, numsteps):
     """ Return a blank pattern of numsteps steps.
 
     """
-
-    pattern_step = [ [ Pixel(Color(0,0,0)) ] * strip.numPixels() ]
-    pattern = [ pattern_step for step in range(numsteps) ]
+    
+    pattern = [ [ Pixel(Color(0,0,0)) for pixel in range(strip.numPixels()) ] for step in range(numsteps) ]
     return pattern
 
 
 def display_pattern(strip):
+    print "STARTING PATTERN (stepping through the door loops for 20 seconds)"
     timestep_ms =  500
 
     pattern = initpattern(strip, 3)
@@ -26,7 +28,11 @@ def display_pattern(strip):
     # At timestep 2, set pixel at position 5 to white
     pattern[2][5].color=Color(255,255,255)
     for step in pattern:
+        print "STEP"
+        print [ id(pixel) for pixel in step ]
         for position, pixel in enumerate(step):
             strip.setPixelColor(position, pixel.color)
+            if position == 5:
+                print "Pixel Color: %s" % pixel.color
         strip.show()
-        time.sleep(timestep_ms/1000)
+        time.sleep(timestep_ms/1000.0)
