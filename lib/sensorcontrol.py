29d18282aa0698bf1d42de1067e87e0f136335f2
Bri <bridotson@gmail.com>
2015-02-14 20:31:19 -0800

    remove unnecessary methods from sensor class

--- lib/sensorcontrol.py
+++ lib/sensorcontrol.py
@@ -24,18 +24,3 @@ class Sensor:
             GPIO.setup(self.pin, GPIO.IN)
         elif self.sensor_type == "button":
             GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-    def check_for_button_press(self, previous_sensor_state, current_sensor_state):
-        d_sensor_state_dt = abs(current_sensor_state - previous_sensor_state)
-        if d_sensor_state_dt > 0:
-            if current_sensor_state == 0:
-                print "button is down"
-            elif current_sensor_state == 1:
-                print "button is released"
-                self.trigger()
-
-    def check_for_motion(self, previous_sensor_state, current_sensor_state):
-        d_sensor_state_dt = current_sensor_state - previous_sensor_state
-        if d_sensor_state_dt > 0:
-            "motion detected!"
-            self.trigger()
