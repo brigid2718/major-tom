6ae2c3732c392d7d68aa42704cfae165d647d659
Ian Dotson <ianm@uw.edu>
2015-02-21 16:44:34 -0800

    Make random raindrops that spread out in a "circle".

--- pattern-lib/seattle.py
+++ pattern-lib/seattle.py
@@ -2,6 +2,7 @@
 
 """
 
+import random
 import pdb
 
 class Pixel():
@@ -18,8 +19,7 @@ def initpattern(strip, numsteps):
     pattern = [ [ Pixel(Color(0,0,0)) for pixel in range(strip.numPixels()) ] for step in range(numsteps) ]
     return pattern
 
-def pulsepixel(pattern, beginstep, position, color):
-    pulse_duration = 20
+def pulsepixel(pattern, beginstep, position, color, pulse_duration = 20):
 
     # We need to unpack the color into RGB
     blue = color & 255
@@ -32,8 +32,6 @@ def pulsepixel(pattern, beginstep, position, color):
         newgreen = int(green * ((i+1)/float(pulse_duration)))
         newblue = int(blue * ((i+1)/float(pulse_duration)))
 
-        step[position].color = Color(newred, newgreen, newblue)
-
     for i, step in enumerate(pattern[beginstep+pulse_duration : beginstep+2*pulse_duration]):
         newred = int(red * ((pulse_duration-i)/float(pulse_duration)))
         newgreen = int(green * ((pulse_duration-i)/float(pulse_duration)))
@@ -41,17 +39,31 @@ def pulsepixel(pattern, beginstep, position, color):
 
         step[position].color = Color(newred, newgreen, newblue)
 
+def makeraindrop(pattern, strip, step, pos, color=Color(0,0,255), fadetime=4):
+    [ pulsepixel(pattern, step, pos, color, fadetime) for i in range(strip.numPixels()) ]
+
+def raindrops(pattern, strip, numdrops=80):
+    while (numdrops >0):
+        step = random.randrange(len(pattern))
+        start_pos = random.randrange(strip.numPixels())
+        drop_pos = [ (start_pos + i) % strip.numPixels() for i in [0,1,-1,2,-2,3,-3,4,-4] ]
+        for pos in drop_pos:
+           #[ pulsepixel(pattern, step, pos, Color(0,0,255), 4) for i in range(strip.numPixels()) ]
+           makeraindrop(pattern, strip, step, pos)
+           # TODO: this shouldn't actually wrap around to step 0
+           step = (step + 1)  % len(pattern)
+        numdrops -= 1
+
 
 def display_pattern(strip):
+    random.seed()
     print "STARTING PATTERN (stepping through the door loops for 20 seconds)"
     timestep_ms =  60
 
     pattern = initpattern(strip, 100)
 
-    # At timestep 2, set pixel at position 5 to white
-    # pattern[2][5].color=Color(255,255,255)
-    # pulsepixel(pattern, 1, 6, Color(0,0,255))
-    [ pulsepixel(pattern, 1, i, Color(0,0,255)) for i in range(strip.numPixels()) ]
+    #[ pulsepixel(pattern, 1, i, Color(0,0,255)) for i in range(strip.numPixels()) ]
+    raindrops(pattern, strip, 10)
     #import IPython; IPython.embed()
     for step in pattern:
         print "STEP"
